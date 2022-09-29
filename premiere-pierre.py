9fec9402044c7976223125e8790f07ef77e69346
Luca <luca.brusadelli@laplateforme.io>
2022-09-29 11:22:09 +0200

    V0

--- premiere-pierre.py
+++ premiere-pierre.py
@@ -1,116 +1,46 @@
-#Labyrinthe
+#Amazing Mazes
+# Première pierre - Recursive Backtrack
 
-#import numpy as np
-#import re
+def init_maze(n):
+    maze = []
 
+    for i in range(n*2+1):
+        maze.append([])
 
-
-maze = []
-n = int(input('Quelle taille de labybrinthe ?'))
-
-for i in range(n*2+1):
-    maze.append([])
-    print(maze)
-    for j in range(n*2+1):
-        maze[i].append([])
-        if (i % 2) == 0:
-            
-            maze[i][j] = '#'
-        else:
-            if (j % 2) == 0:
+        for j in range(n*2+1):
+            maze[i].append([])
+            if (i % 2) == 0:
+                
                 maze[i][j] = '#'
             else:
-                maze[i][j] = '.'
+                if (j % 2) == 0:
+                    maze[i][j] = '#'
+                else:
+                    maze[i][j] = '.'
+    
+    return maze
 
-print(maze)
+def str_maze(maze):
+    
+    maze_out = ''
+    
+    for i in range(len(maze)):
 
-fichier = open("output_maze.txt","w")
-fichier.write(str(maze))
+        if i != 0:
+            maze_out = maze_out + '\n'
 
-# pattern = '.'
-# for i in range(ligne):
-#     maze_col.append(re.findall(pattern,maze_lg[i]))
-# # print('maze_col',maze_col)
-
-# col = len(maze_col[0])
-
-# #fonction qui teste la possibilité d'une position en fonction de la liste des positions des dames précédentes
-# def possible(lgn,ls_pos):
-#     if len(ls_pos) < 1:
-#         return True
-#     else:    
-#         for i in range(len(ls_pos)):
-#             diff_col = len(ls_pos) - i
-
-#             if ls_pos[i] == lgn:
-#                 return False
-#             elif ls_pos[i] == lgn - diff_col:
-#                 return False    
-#             elif ls_pos[i] == lgn + diff_col:
-#                 return False
-
-#         return True
-
-# # maze2d = np.array([])
-
-# # for i in range(ligne):
-# #     maze2d = np.append(maze2d, maze_lg[i])
-# #     print('maze2d',maze2d,len(maze2d))
-# #     for j in range(col):
-# #         maze2d = np.append(maze2d, maze_lg[i], axis=0)
-# #         print('maze2d',maze2d)
-# # print('maze2d',maze2d)
-
-# # #création classe Board pour visualiser les solutions
-# # class Board:
-# #     def __init__(self,n):
-# #         self.plateau = np.zeros([n, n], dtype = str)
-# #         for i in range(n):
-# #             for j in range(n):
-# #                 self.plateau[i,j] = 'O'
-# #         self.taille = n
-
-# #     def Print(self):
-# #         print(self.plateau)
-
-# #     def Visualiser(self,positions):
-# #         for i in range(len(positions)):
-# #             self.plateau[i,positions[i]] = 'X'
+        for j in range(len(maze[i])):
+            maze_out = maze_out + maze[i][j]
+    
+    return maze_out
 
 
+def write_maze(maze,file_name):
+    fichier = open(file_name + '.txt',"w")
+    fichier.write(str_maze(maze))
 
-# #fonction de résolution en fonction de la taille du plateau
-# def resolution(taille,col):
-#     solutions = []
+maze1 = init_maze(int(input('Quelle taille de labybrinthe ?')))
 
-#     if col == 1:
-#         for lig in range(taille):
-#             solutions.append([lig])
+print(str_maze(maze1))
 
-#             if lig == taille - 1:
-#                 return solutions
-#     else:
-#         solutions_i = resolution(taille,col-1)
-#         len_i = len(solutions_i)
-
-#         for sol in range(len_i):
-#             for lig in range(taille):
-#                 if possible(lig,solutions_i[sol]) == True:
-#                     solutions.append(solutions_i[sol] + [lig])
-
-#             if sol == len_i - 1:
-#                 return solutions
-
-# #fonction d'affichage du résultat
-# def affichage(solutions):
-#     if len(solutions) == 0:
-#         print('Il n''y a pas de solutions',solutions)
-#     else:
-#         print('Il y a ' + str(len(solutions)) + ' solutions.','Les solutions sont :')
-#         taille = len(solutions[0])
-#         for i in range(len(solutions)):
-#             resultat = Board(taille)
-#             resultat.Visualiser(solutions[i])
-#             print('\n' + 'Solution ' + str(i+1))
-#             resultat.Print()
-
+write_maze(maze1,input('Nom du fichier ?'))
