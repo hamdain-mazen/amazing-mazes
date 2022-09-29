e7cc336c86c4910f5cac043f75dea4a00e09eff9
Luca <luca.brusadelli@laplateforme.io>
2022-09-29 13:38:47 +0200

    V0 avec main

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from premiere import *
 
 #Crée un labyrinthe selon la taille saisie
+# maze1 = Maze(int(input('Quelle taille de labybrinthe ?')))
 
-maze1 = Maze(int(input('Quelle taille de labybrinthe ?')))
+#Affiche le labyrinthe dans la console
 # maze1.Print()
 
 #Ecrit le labyrinthe dans un fichier dont le nom est à saisir
-maze1.Write(input('Nom du fichier ?'))
+# maze1.Write(input('Nom du fichier ?'))
 
-#Crée un labyrinthe de taille 6
-# maze1 = init_maze(6)
-# print(str_maze(maze1))
+# #Crée un labyrinthe de taille 6
+maze1 = Maze(6)
+maze1.Print()
 
 #Choisit un cheminement de 3 cellules depuis la première et affiche leur index et leur état
 # cell1 = Cellule(maze1, 1, 1)
@@ -20,3 +21,12 @@ maze1.Write(input('Nom du fichier ?'))
 # cell1.Print()
 # cell2.Print()
 # cell3.Print()
+
+# chemin = []
+# i = 0
+# if i == 0:
+#     cellule = Cellule(maze1, 1, 1)
+#     chemin[i] = cellule.ID()
+#     cellule.visited = True
+#     i = i + 1
+# else:
--- premiere.py
+++ premiere.py
@@ -1,81 +1,116 @@
 #Amazing Mazes
+
 # Première pierre - Recursive Backtrack
+
 
 import random
 
+
 class Maze:
-    def __init__(self,n):
-        self.n = n
+
+    def __init__(self,N):
+        self.N = N
+        self.n = 2 * N + 1
         self.maze_matrix = []
 
-        for i in range(n*2+1):
+        for i in range(self.n):
             self.maze_matrix.append([])
 
-            for j in range(n*2+1):
+            for j in range(self.n):
                 self.maze_matrix[i].append([])
+
                 if (i % 2) == 0:
-                    
                     self.maze_matrix[i][j] = '#'
+
                 else:
                     if (j % 2) == 0:
                         self.maze_matrix[i][j] = '#'
+
                     else:
                         self.maze_matrix[i][j] = '.'
+
 
         self.maze_matrix[1][0] = '.'
         self.maze_matrix[-2][-1] = '.'
 
+
     def str_maze(self):
         
         maze_out = ''
-        T = 2 * self.n + 1 
-        for i in range(T):
+
+        for i in range(self.n):
 
             if i != 0:
+
                 maze_out = maze_out + '\n'
 
-            for j in range(T):
+
+            for j in range(self.n):
+
                 maze_out = maze_out + self.maze_matrix[i][j]
         
+
         return maze_out
 
+
     def Print(self):
+
         print(self.str_maze())
 
+
     def Write(self,file_name):
+
         fichier = open(file_name + '.txt',"w")
+
         fichier.write(self.str_maze())
 
+
 class Cellule:
-    def __init__(self,maze,x,y):
-        self.x = x
-        self.y = y
-        self.droite = maze[2 * self.x - 1][(2 * self.y - 1) + 1]
-        self.gauche = maze[2 * self.x - 1][(2 * self.y - 1) - 1]
-        self.haut = maze[(2 * self.x - 1) - 1][2 * self.y - 1]
-        self.bas = maze[(2 * self.x - 1) + 1][2 * self.y - 1]
+
+    def __init__(self,maze,X,Y):
+        self.X = X
+        self.x = 2 * self.X - 1
+        self.Y = Y
+        self.y = 2 * self.Y - 1
+
+        self.droite = maze[x][y + 1]
+        self.gauche = maze[x][y - 1]
+        self.haut = maze[x - 1][y]
+        self.bas = maze[x + 1][y]
+
         self.maze = maze
         self.visited = False
 
     def Print(self):
-        print('x',self.x,'y',self.y)
+
+        print('X',self.X,'Y',self.Y)
+
         print(' ' + self.haut)
-        print(self.gauche + ' ' + self.droite)        
+        print(self.gauche + ' ' + self.droite)
         print(' ' + self.bas)
+    
+
+    def ID(self):
+        return [self.X, self.Y]
+
 
     def Suiv(self):
         dir = random.randint(1,4)
 
-        if dir == 1 and self.x > 1:
-            return Cellule(self.maze,self.x-1,self.y)
+        if dir == 1 and self.X > 1:
+            return Cellule(self.maze,self.X-1,self.Y)
 
-        elif dir == 2 and self.y < len(self.maze)//2 :
-            return Cellule(self.maze,self.x,self.y+1)
+        elif dir == 2 and self.Y < len(self.maze)//2 :
+            return Cellule(self.maze,self.X,self.Y+1)
 
-        elif dir == 3 and self.x < len(self.maze)//2 :
-            return Cellule(self.maze,self.x+1,self.y)
+        elif dir == 3 and self.X < len(self.maze)//2 :
+            return Cellule(self.maze,self.X+1,self.Y)
 
-        elif dir == 4 and self.y > 1:
-            return Cellule(self.maze,self.x,self.y-1)
-        
+        elif dir == 4 and self.Y > 1:
+            return Cellule(self.maze,self.X,self.Y-1)
+
         return self.Suiv()
+
+    def Visite(self):
+        self.visited = True
+        self.maze.maze_matrix[self.x, self.y] = '+'
