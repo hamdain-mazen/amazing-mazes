48cb871dbe49c9acfaff8792d57c1998ae41d373
Luca <luca.brusadelli@laplateforme.io>
2022-09-30 09:53:09 +0200

    test push branch

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 from premiere import *
 
-#Create a maze depending on the size we wish
+#Crée un labyrinthe selon la taille saisie
 # maze1 = Maze(int(input('Input maze size :')))
 
-#Show the maze on the terminal
+#Affiche le labyrinthe dans la console
 # maze1.Print()
 
 #Ecrit le labyrinthe dans un fichier dont le nom est à saisir
-#Write the maze in a file named by input
 # maze1.Write(input('File name ?'))
 
-#Create a maze of size 6
+# #Crée un labyrinthe de taille 6
 maze1 = Maze(6)
 maze1.Print()
 
 #Choisit un cheminement de 3 cellules depuis la première et affiche leur index et leur état
-#Choose a movement of three cells starting from the origin and show their index and status
 # cell1 = Cell(maze1, 1, 1)
 # cell2 = cell1.Next()
 # cell3 = cell2.Next()
@@ -27,19 +25,44 @@ maze1.Print()
 path = []
 i = 0
 END = False
-while END  == False:
+while i < 10:
     if i == 0:
         cell = Cell(maze1, 1, 1)
         path.append(cell.ID())
         cell.Visit()
         i = i + 1
-        print('\n')
         maze1.Print()
     
     else:
         cell = cell.Next()
         path.append(cell.ID())
         cell.Visit()
+        maze1.Print()
+        print(path)
+
+        if cell.x == path[-2][0]:
+            if cell.y > path[-2][1]:
+                dir = 4
+            else:
+                dir = 2
+
+        elif cell.y == path[-2][1]:
+            if cell.x > path[-2][0]:
+                dir = 1
+            else:
+                dir = 3
+
+        if dir == 1:
+            maze1.maze_matrix[cell.x-1][cell.y] = "|"
+
+        elif dir == 2:
+            maze1.maze_matrix[cell.x][cell.y+1] = "-"
+
+        elif dir == 3:
+            maze1.maze_matrix[cell.x+1][cell.y] = "|"
+
+        elif dir == 4:
+            maze1.maze_matrix[cell.x][cell.y-1] = "-"
+
         i = i + 1
-        print('\n')
         maze1.Print()
